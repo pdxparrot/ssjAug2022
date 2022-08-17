a8b383594882963021b1184c8700028c194842c5
Shane Lillie <ignignokterr@gmail.com>
2022-08-16 21:03:21 -0700

    Helper for dealing with animations

---
+++ scripts/Model.cs
@@ -0,0 +1,18 @@
+using Godot;
+
+namespace pdxpartyparrot.ssjAug2022
+{
+    public class Model : Node
+    {
+        private AnimationTree _animationTree;
+
+        #region Godot Lifecycle
+
+        public override void _Ready()
+        {
+            //_animationTree = GetNode<AnimationTree>();
+        }
+
+        #endregion
+    }
+}
