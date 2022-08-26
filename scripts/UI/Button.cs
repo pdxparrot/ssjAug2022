bf0b3c838a08a8590ede1558fe99fffe910af46e
Shane Lillie <ignignokterr@gmail.com>
2022-08-25 19:23:07 -0700

    Make buttons their own scene
    Button audio

---
+++ scripts/UI/Button.cs
@@ -0,0 +1,33 @@
+using Godot;
+
+using pdxpartyparrot.ssjAug2022.Managers;
+
+namespace pdxpartyparrot.ssjAug2022.UI
+{
+    public class Button : TextureButton
+    {
+        // TODO: not sure what signal to use here
+        private AudioStreamPlayer _hoverAudioStreamPlayer;
+
+        private AudioStreamPlayer _pressedAudioStreamPlayer;
+
+        #region Godot Lifecycle
+
+        public override void _Ready()
+        {
+            _hoverAudioStreamPlayer = GetNode<AudioStreamPlayer>("Hover");
+            _pressedAudioStreamPlayer = GetNode<AudioStreamPlayer>("Pressed");
+        }
+
+        #endregion
+
+        #region Signal Handlers
+
+        private void _on_Button_pressed()
+        {
+            _pressedAudioStreamPlayer.Play();
+        }
+
+        #endregion
+    }
+}
