a9e7367e46c9f33d737a2225fec4a8940fa66263
Shane Lillie <ignignokterr@gmail.com>
2022-08-29 19:47:39 -0700

    Show the pigsquad bumper

--- scripts/SplashScreen.cs
+++ scripts/SplashScreen.cs
@@ -3,13 +3,14 @@ using Godot;
 using System.Threading.Tasks;
 
 using pdxpartyparrot.ssjAug2022.Managers;
+using pdxpartyparrot.ssjAug2022.Util;
 
 namespace pdxpartyparrot.ssjAug2022
 {
     public class SplashScreen : Node
     {
         [Export]
-        private Image[] _splashImages = new Image[0];
+        private Texture[] _splashImages = new Texture[0];
 
         private TextureRect _splashImage;
 
@@ -35,6 +36,10 @@ namespace pdxpartyparrot.ssjAug2022
                 await ShowNextSplashImageAsync().ConfigureAwait(false);
                 return;
             }
+
+            if(!_fadeTimer.IsStopped()) {
+                _splashImage.Modulate = new Color(1.0f, 1.0f, 1.0f, _fadeTimer.TimeLeft / _fadeTimer.WaitTime);
+            }
         }
 
         #endregion
@@ -45,9 +50,34 @@ namespace pdxpartyparrot.ssjAug2022
                 QueueFree();
 
                 await SceneManager.Instance.LoadMainMenuAsync().ConfigureAwait(false);
+                return;
             }
+
+            // TODO: this is just a jank hack for ssj2022, please remove it
+            if(_currentSplashImage == 1 && _splashImages.Length > 2) {
+                _currentSplashImage = 1 + PartyParrotManager.Instance.Random.CoinFlip();
+            }
+
+            _splashImage.Texture = _splashImages[_currentSplashImage];
+            _splashImage.Modulate = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+
+            _displayTimer.Start();
 
             _currentSplashImage++;
         }
+
+        #region Signals
+
+        private void _on_Timer_timeout()
+        {
+            _fadeTimer.Start();
+        }
+
+        private void _on_Fade_Timer_timeout()
+        {
+            _splashImage.Modulate = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+        }
+
+        #endregion
     }
 }
