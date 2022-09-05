b773a4da5adb09c7d7dcafea75171f29e4748282
Shane Lillie <ignignokterr@gmail.com>
2022-09-05 09:26:13 -0700

    Hook up new human model

--- scripts/NPCs/Human/Human.cs
+++ scripts/NPCs/Human/Human.cs
@@ -98,6 +98,8 @@ namespace pdxpartyparrot.ssjAug2022.NPCs.Human
         {
             base.OnDied();
 
+            Model.ChangeState("death");
+
             _stateMachine.ChangeState(new States.Dead());
 
             _deathAudioPlayer.Play();
@@ -133,7 +135,7 @@ namespace pdxpartyparrot.ssjAug2022.NPCs.Human
             }
 
             GD.Print($"[{Id}] Attack!");
-            Model.TriggerOneShot("parameters/Claw_AttackTrigger/active");
+            Model.TriggerOneShot("parameters/BlendTree/Claw_Attack_Trigger/active");
             _attackAudioPlayer.Play();
 
             _attackAnimationTimer.Start();
