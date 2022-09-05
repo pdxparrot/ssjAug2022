65d9f3fc04ad56015123ab9f5fbc6bbd1dfa3674
Shane Lillie <ignignokterr@gmail.com>
2022-09-04 22:34:52 -0700

    This is so jank but make sure enemies look at the player when attacking

--- scripts/NPCs/Boss/States/AttackPlayer.cs
+++ scripts/NPCs/Boss/States/AttackPlayer.cs
@@ -43,6 +43,7 @@ namespace pdxpartyparrot.ssjAug2022.NPCs.Boss.States
                 return;
             }
 
+            owner.Pivot.LookAt(Target.GlobalTranslation, Vector3.Up);
             owner.Attack();
         }
 
