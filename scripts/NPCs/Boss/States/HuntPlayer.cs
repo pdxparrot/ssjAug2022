24154283dd1967a5d5b518755fa226c22e9ec3a3
Shane Lillie <ignignokterr@gmail.com>
2022-09-06 19:21:15 -0700

    Add a little bit of an offset when hunting the player

--- scripts/NPCs/Boss/States/HuntPlayer.cs
+++ scripts/NPCs/Boss/States/HuntPlayer.cs
@@ -3,6 +3,7 @@ using Godot;
 using pdxpartyparrot.ssjAug2022.Collections;
 using pdxpartyparrot.ssjAug2022.Managers;
 using pdxpartyparrot.ssjAug2022.NPCs.AI;
+using pdxpartyparrot.ssjAug2022.Util;
 
 namespace pdxpartyparrot.ssjAug2022.NPCs.Boss.States
 {
@@ -16,6 +17,15 @@ namespace pdxpartyparrot.ssjAug2022.NPCs.Boss.States
         {
             var target = PlayerManager.Instance.Players.GetRandomEntry();
             _target = target.Value.GlobalTranslation;
+
+            // fuzz the target a little bit so we don't beeline the player
+            var offset = new Vector3(
+                            PartyParrotManager.Instance.Random.NextSingle(-5.0f, 5.0f),
+                            0.0f,
+                            PartyParrotManager.Instance.Random.NextSingle(-5.0f, 5.0f)
+                        );
+            _target += offset;
+
             GD.Print($"[{owner.Id}] hunting player {target.Value.Name} at {_target}");
 
             _lastTargetUpdate = Time.GetTicksMsec();
